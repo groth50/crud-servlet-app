b23ddf70e9b73ff1a4b398149fb76d28e9cd1934
groth50 <xgrothx@gmail.com>
2018-12-26 20:41:29 +0300

    Refactoring

--- src/main/java/database/ExecuteCallable.java
+++ src/main/java/database/ExecuteCallable.java
@@ -3,7 +3,7 @@ package database;
 import javax.persistence.EntityManager;
 
 /**
- * Represent query that can returns a result and may throw an exception.
+ * Represent query that can returns a result.
  * Implementors define a single method with single
  * argument {@link EntityManager} called {@code execute}.
  *
@@ -12,14 +12,11 @@ import javax.persistence.EntityManager;
 @FunctionalInterface
 public interface ExecuteCallable<T> {
     /**
-     * Use for implements query from client code, can return a result,
-     * or throws an exception if unable to do so.
+     * Use for implements query from client code, can return a result.
      *
      * @param entityManager {@link EntityManager} use for query
      *
      * @return T type of result query
-     *
-     * @throws Exception if unable to execute query
      */
     T execute(EntityManager entityManager) throws Exception;
 }
