8d57d89ef530ab7ba3e30948d06f9c395c011f72
groth50 <xgrothx@gmail.com>
2018-12-26 20:19:58 +0300

    Add comments

--- src/main/java/database/Executor.java
+++ src/main/java/database/Executor.java
@@ -11,18 +11,38 @@ import javax.persistence.EntityTransaction;
  * class provides a way of decoupling queries submission from the
  * mechanics of how each queries will be run, including details of open and close
  * resources, way to database connect, etc. An Executor is normally used lambdas
- * from Java 1.8
+ * from Java 1.8 for callback.
  *
  * @autor  Alex
  */
 public class Executor {
+
+    /** Standard logger */
     static final Logger LOGGER = LogManager.getLogger(Executor.class.getName());
+
+    /**
+     * Object used to interact with the entity manager factory
+     * for the persistence unit.
+     *
+     * @see EntityManagerFactory
+     * @see EntityManager
+     */
     private final EntityManagerFactory entityManagerFactory;
 
     public Executor(EntityManagerFactory entityManagerFactory) {
         this.entityManagerFactory = entityManagerFactory;
     }
 
+    /**
+     * Execute a {@link ExecuteCallable} in transaction, use for
+     * data manipulation query and another, that need transaction.
+     * If transaction fail, call rollback.
+     *
+     * @param callable - ExecuteCallable represents query
+     *
+     * @param <T> - this type return {@link ExecuteCallable#execute(EntityManager)}.
+     *              Should return int i = 0 in this case and client just ignore this
+     */
     public <T> void doTransaction(ExecuteCallable<T> callable) {
         EntityManager entityManager = null;
         EntityTransaction transaction = null;
@@ -45,7 +65,16 @@ public class Executor {
         }
     }
 
-    //через замыкание
+    /**
+     * Execute a {@link ExecuteCallable}, use for
+     * data query. Don't use transactions.
+     *
+     * @param callable - ExecuteCallable represents query
+     *
+     * @param <T> - this type return {@link ExecuteCallable#execute(EntityManager)}.
+     *
+     * @return T - query result
+     */
     public <T> T doQuery(ExecuteCallable<T> callable) {
         T result = null;
         EntityManager entityManager = null;
