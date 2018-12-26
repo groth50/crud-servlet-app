a0b46f4d26cefe0c13be4a071ddadd3d4b481686
groth50 <xgrothx@gmail.com>
2018-12-26 20:26:54 +0300

    Add annotation

--- src/main/java/database/ExecuteCallable.java
+++ src/main/java/database/ExecuteCallable.java
@@ -3,6 +3,7 @@ package database;
 import javax.persistence.EntityManager;
 
 //todo: разделить на два? Один для транзакций, другой чисто для запросов?
+@FunctionalInterface
 public interface ExecuteCallable<T> {
     T execute(EntityManager entityManager);
 }
