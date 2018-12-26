663ab62490f5e16061a9e51d4c6358a1fe008f57
groth50 <xgrothx@gmail.com>
2018-12-26 20:40:18 +0300

    Add comments

--- src/main/java/database/ExecuteCallable.java
+++ src/main/java/database/ExecuteCallable.java
@@ -2,8 +2,24 @@ package database;
 
 import javax.persistence.EntityManager;
 
-//todo: разделить на два? Один для транзакций, другой чисто для запросов?
+/**
+ * Represent query that can returns a result and may throw an exception.
+ * Implementors define a single method with single
+ * argument {@link EntityManager} called {@code execute}.
+ *
+ * @param <T> type of return result
+ */
 @FunctionalInterface
 public interface ExecuteCallable<T> {
-    T execute(EntityManager entityManager);
+    /**
+     * Use for implements query from client code, can return a result,
+     * or throws an exception if unable to do so.
+     *
+     * @param entityManager {@link EntityManager} use for query
+     *
+     * @return T type of result query
+     *
+     * @throws Exception if unable to execute query
+     */
+    T execute(EntityManager entityManager) throws Exception;
 }
