05c5d6e97b4a530c42ff0c277f489193121decf4
groth50 <xgrothx@gmail.com>
2018-12-27 20:21:57 +0300

    Add comments

--- src/main/java/accounts/FactoryAccountService.java
+++ src/main/java/accounts/FactoryAccountService.java
@@ -2,9 +2,22 @@ package accounts;
 
 import database.DBService;
 
+/**
+ * Class used for return instance {@link AccountService}
+ *
+ * @autor  Alex
+ */
 public class FactoryAccountService {
+
+    /** Database instance for {@link AccountService} interface */
     public static final AccountServiceDB accountServiceDB = new AccountServiceDB(new DBService());
 
+    /**
+     * Factory method, return instance
+     * for {@link AccountService} interface
+     *
+     * @return instance for AccountService interface
+     */
     public static AccountService getAccountService() {
         return accountServiceDB;
     }
