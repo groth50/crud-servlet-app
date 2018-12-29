03e502e1f205bc5d0cd484988b564f076c629ddc
groth50 <xgrothx@gmail.com>
2018-12-29 16:41:15 +0300

    Add comments

--- src/main/java/filter/SecurityFilter.java
+++ src/main/java/filter/SecurityFilter.java
@@ -3,41 +3,59 @@ package filter;
 import accounts.AccountService;
 import accounts.FactoryAccountService;
 import accounts.UserAccount;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import servlet.GetMainMenuServlet;
 import servlet.SignInServlet;
 import utils.SecurityUtil;
-
+import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
- * Intercept HTTP requests, check access rights
+ * Intercept HTTP requests, check authentication
+ * and authorization - access rights
  * for requestor and permit or denied access.
  *
  * @autor  Alex
+ * @see    Filter
  */
 @WebFilter(filterName = "Security", urlPatterns = {"/signup", "/signin", "/mainmenu", "/adminmenu", "/adduser", "/deleteuser", "/updateuser", "/userinfo"})
 public class SecurityFilter implements Filter {
-    private AccountService accountService;
-    static final Logger LOGGER = LogManager.getLogger(SecurityFilter.class.getName());
+
+    /** Standard logger */
+    private static final Logger LOGGER = LogManager.getLogger(SecurityFilter.class.getName());
+
+    /** Checked requests */
     public static final String[] URLS = {"/signup", "/signin", "/mainmenu", "/adminmenu", "/adduser", "/deleteuser", "/updateuser", "/userinfo"};
 
+    /** Managing user accounts and their sessions in application */
+    private AccountService accountService;
 
+    /**
+     * Initialization method
+     *
+     * @param filterConfig see {@link Filter#init(FilterConfig)}
+     */
     @Override
     public void init(FilterConfig filterConfig) {
         this.accountService = FactoryAccountService.getAccountService();
     }
 
+    /**
+     * Close resources, see {@link Filter#destroy()}
+     */
     @Override
     public void destroy() {
         this.accountService = null;
     }
 
+    /**
+     * Check authentication and authorization
+     * See {@link Filter#doFilter(ServletRequest, ServletResponse, FilterChain)}
+     */
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         LOGGER.debug("doFilter from " + this.getClass().getSimpleName());
@@ -48,7 +66,7 @@ public class SecurityFilter implements Filter {
         UserAccount loginedUser = accountService.getUserBySessionId(request.getSession().getId());
         boolean isLoginPage = SecurityUtil.isLoginPage(servletPath);
 
-        //check log in
+        // check authentication
         if (loginedUser == null && isLoginPage) {
             chain.doFilter(request, response);
             return;
@@ -65,6 +83,7 @@ public class SecurityFilter implements Filter {
             return;
         }
 
+        // check authorization
         UserAccount.Role role = loginedUser.getRole();
         if (SecurityUtil.hasPermission(servletPath, role)) {
             chain.doFilter(request, response);
