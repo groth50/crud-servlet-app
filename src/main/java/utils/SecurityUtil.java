fc5c9b7b8217d303d8fcea68605111b4ce1f4924
groth50 <xgrothx@gmail.com>
2018-12-30 13:05:27 +0300

    Add comments

--- src/main/java/utils/SecurityUtil.java
+++ src/main/java/utils/SecurityUtil.java
@@ -1,19 +1,40 @@
 package utils;
+
 import accounts.UserAccount;
 import configs.SecurityConfig;
-
 import java.util.List;
 
+/**
+ * Helper for {@link filter.SecurityFilter}
+ *
+ * @autor  Alex
+ */
 public class SecurityUtil {
 
-    // Проверить имеет ли данный 'request' подходящую роль?
+    /**
+     * Check access for role, return boolean.
+     * True if allowed, false if denied
+     *
+     * @param servletPath servlet path from URI
+     *
+     * @param role user role {@link UserAccount.Role}
+     *
+     * @return true if allowed, false if denied
+     */
     public static boolean hasPermission(String servletPath, UserAccount.Role role) {
 
         List<String> servletPathForRole = SecurityConfig.getServletPathForRole(role);
         return servletPathForRole.contains(servletPath);
     }
 
-    //проверка - это страница для входа или регистрации?
+    /**
+     * Checks if the servlet path
+     * is registration or login page
+     *
+     * @param servletPath servlet path from URI
+     *
+     * @return true if yes, false if not
+     */
     public static boolean isLoginPage(String servletPath) {
         return (servletPath.equals("/signin") || servletPath.equals("/signup"));
     }
