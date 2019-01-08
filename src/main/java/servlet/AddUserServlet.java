75445786b6ab1edf9b1712baf7d192e871bc2438
groth50 <xgrothx@gmail.com>
2019-01-08 11:22:51 +0300

    Change access modifier

--- src/main/java/servlet/AddUserServlet.java
+++ src/main/java/servlet/AddUserServlet.java
@@ -22,7 +22,7 @@ import org.apache.logging.log4j.Logger;
 public class AddUserServlet extends HttpServlet {
 
     /** Standard logger */
-    static final Logger LOGGER = LogManager.getLogger(AddUserServlet.class.getName());
+    private static final Logger LOGGER = LogManager.getLogger(AddUserServlet.class.getName());
 
     /**
      * A JSP filename to which that servlet forwards
