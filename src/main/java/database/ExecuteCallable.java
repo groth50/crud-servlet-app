2912b90d72984885e9c2af285ca94d36740150e6
groth50 <xgrothx@gmail.com>
2019-01-10 13:38:30 +0300

    Add comments

--- src/main/java/database/ExecuteCallable.java
+++ src/main/java/database/ExecuteCallable.java
@@ -7,18 +7,16 @@ import javax.persistence.EntityManager;
  * Implementors define a single method with single
  * argument {@link EntityManager} called {@code execute}.
  *
- * @param <T> type of return result
- *
- * @autor  Alex
+ * @autor Alex
+ * @param <T> type of return query result
  */
 @FunctionalInterface
 public interface ExecuteCallable<T> {
     /**
      * Use for implements query from client code, can return a result.
      *
-     * @param entityManager {@link EntityManager} use for query
-     *
-     * @return <T> type of result query
+     * @param entityManager {@link EntityManager} used to query
+     * @return <T> type of return query result
      */
     T execute(EntityManager entityManager);
 }
